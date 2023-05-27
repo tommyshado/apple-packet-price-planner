6f9a9894747b1dbbda048ac07a33acbfd2d10b4e
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-27 16:53:35 +0200

    store the cost of an apple in the local storage and made the cost of an apple visible when the client refreshes the page

--- dom/apple-packet-price-planner.dom.js
+++ dom/apple-packet-price-planner.dom.js
@@ -8,6 +8,14 @@ const priceOfApple = document.querySelector('.applePrice');
 // buttons
 const calculateBtn = document.querySelector('.calculateBtn');
 
+// variables
+let appleCost = 0;
+
+if (localStorage['costOfApple']) {
+    priceOfApple.innerText = localStorage.getItem('costOfApple');
+    appleCost = priceOfApple.innerText;
+}
+
 // factory function instance
 const apple = appleApp();
 
@@ -16,4 +24,5 @@ calculateBtn.addEventListener('click', function() {
     apple.getAppleBoxPrice(costOfPacketApples.value);
     apple.getNumberForApples(numApplesInBox.value);
     priceOfApple.innerText = apple.costOfApple();
+    localStorage.setItem('costOfApple', priceOfApple.innerHTML);
 });
