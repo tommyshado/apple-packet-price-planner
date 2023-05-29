5e46ed944c35f0a1ae426cece7e367d66c83896c
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-29 09:25:58 +0200

    used es 6 to write my callback function

--- test/apple-packet-price-planner.test.js
+++ test/apple-packet-price-planner.test.js
@@ -1,4 +1,4 @@
-describe('The Apple packet price planner factory function', function() {
+describe('The Apple packet price planner factory function', () => {
 
     it('should be able to get the price of the apples per box & get the number of the apples per box and returns the cost per apple', function() {
         const apple = appleApp();
