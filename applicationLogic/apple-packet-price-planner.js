7ebdd90b9f7c20c818eac72317c8c37a95ee9bcb
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-29 09:24:12 +0200

    added a global variable for the cost of an apple

--- applicationLogic/apple-packet-price-planner.js
+++ applicationLogic/apple-packet-price-planner.js
@@ -1,11 +1,14 @@
-function appleApp() {
+const appleApp = () => {
     let appleBoxCost = 0;
     let appleNumber = 0;
+    let appleCost = 0;
 
-    function costOfApple(appleBoxPrice, numberOfApples) {
+    const costOfApple = (appleBoxPrice, numberOfApples) => {
         appleBoxCost = Number(appleBoxPrice);
         appleNumber = Number(numberOfApples);
-        return (appleBoxCost / appleNumber).toFixed(2);
+        appleCost = (appleBoxCost / appleNumber).toFixed(2);
+        
+        return appleCost;
     }
 
     return {
