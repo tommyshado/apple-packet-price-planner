23f66e23f524085634a89e8d4084217da3c16d69
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-29 10:27:10 +0200

    tested my javaScript logic for the costOfPacket method

--- test/apple-packet-price-planner.test.js
+++ test/apple-packet-price-planner.test.js
@@ -1,7 +1,24 @@
-describe('The Apple packet price planner factory function', () => {
+describe('The Apple packet price planner factory function', function() {
+    it('should be able to get the price of the apples per box', function() {
+        const apple = appleApp();
+        assert.equal(apple.getAppleBoxPrice(20), 20);
+    });
+
+    it('should be able to get the number of the apples per box', function() {
+        const apple = appleApp();
+        assert.equal(apple.getNumberForApples(10), 10);
+    });
 
     it('should be able to get the price of the apples per box & get the number of the apples per box and returns the cost per apple', function() {
         const apple = appleApp();
-        assert.equal(apple.costOfApple(20, 10), 2);
+        apple.getAppleBoxPrice(20);
+        apple.getNumberForApples(10);
+        assert.equal(apple.costOfApple(), 2);
+    });
+
+    it('should be able to calculate the cost of an apple packet', function() {
+        const apple = appleApp();
+        apple.getAppleBoxPrice(50);
+        assert.equal(apple.costOfPacket(10), 5);
     });
 })
