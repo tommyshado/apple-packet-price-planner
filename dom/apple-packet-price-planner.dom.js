a702b6543c99ce6afe49bbab9174be773a9ed379
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-29 10:26:33 +0200

    linked my costOfPacket method to the dom elements

--- dom/apple-packet-price-planner.dom.js
+++ dom/apple-packet-price-planner.dom.js
@@ -1,14 +1,16 @@
 // input elements
 const costOfPacketApples = document.querySelector('.packetCostApples');
 const numApplesInBox = document.querySelector('.applesInBox');
+const applePacketSize = document.querySelector('.applesPacket');
 
 // output elements
 const priceOfApple = document.querySelector('.applePrice');
+const priceOfPacket = document.querySelector('.applePacketPrice');
 
 // buttons
 const calculateBtn = document.querySelector('.calculateBtn');
 
-// variables
+// variables of the local storage
 let appleCost = 0;
 
 if (localStorage['costOfApple']) {
@@ -17,14 +19,35 @@ if (localStorage['costOfApple']) {
     appleCost = priceOfApple.innerText;
 }
 
+let applePacketCost = 0;
+
+if (localStorage['costPerPacket']) {
+    priceOfPacket.innerText = localStorage.getItem('costPerPacket');
+    priceOfPacket.classList.add('output');
+    applePacketCost = priceOfPacket.innerText;
+}
+
 // factory function instance
 const apple = appleApp();
 
 // event listeners
 calculateBtn.addEventListener('click', function() {
-    priceOfApple.innerText = apple.costOfApple(costOfPacketApples.value, numApplesInBox.value);
+
+    // cost of an apple
+    apple.getAppleBoxPrice(costOfPacketApples.value);
+    apple.getNumberForApples(numApplesInBox.value);
+    priceOfApple.innerText = apple.costOfApple();
     priceOfApple.classList.add('output');
+    localStorage.setItem('costOfApple', priceOfApple.innerText);
+
+    // cost of apple per packet
+    apple.getAppleBoxPrice(costOfPacketApples.value);
+    priceOfPacket.innerText = apple.costOfPacket(applePacketSize.value);
+    priceOfPacket.classList.add('output');
+    localStorage.setItem('costPerPacket', priceOfPacket.innerText);
+
+    // setting input elements to an empty string
     costOfPacketApples.value = '';
     numApplesInBox.value = '';
-    localStorage.setItem('costOfApple', priceOfApple.innerHTML);
+    applePacketSize.value = '';
 });
